ce1804878ec9545d6014e62331646cf0aa42abdc
matteo-luraghi <matteoluraghi50@gmail.com>
2025-03-31 15:43:37 +0200

    [ADD] q7 done

--- Challenge 2/src/q7.py
+++ Challenge 2/src/q7.py
@@ -3,12 +3,16 @@ import pyshark
 def answer_q7():
 
     # Load the .pcapng file and apply a mqtt display filter
-    packets = pyshark.FileCapture("./docs/challenge2.pcapng")
+    packets = pyshark.FileCapture("./docs/challenge2.pcapng", display_filter="udp")
+
+    #TODO:in the report say that since this is 0 it isn't necessary to filter more
 
     ########################### Process requests
+    count = 0
     for pkt in packets:
         if not hasattr(pkt, "udp"):
             continue
 
         if int(pkt.udp.dstport) == 1885:
-            print(dir(pkt))
+            count += 1
+    print(count)
