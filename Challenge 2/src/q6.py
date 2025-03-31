db0e6e50b1acf53e9d29c469b9dce2ac36893195
matteo-luraghi <matteoluraghi50@gmail.com>
2025-03-31 15:54:54 +0200

    [ADD] q6 done

--- Challenge 2/src/q6.py
+++ Challenge 2/src/q6.py
@@ -5,26 +5,10 @@ def answer_q6():
     print("QUESTION 6")
 
     # Load the .pcapng file and apply a mqtt display filter
-    packets = pyshark.FileCapture("./docs/challenge2.pcapng", display_filter="mqtt")
+    packets = pyshark.FileCapture("./docs/challenge2.pcapng", 
+        display_filter="mqtt.msgtype==3 && mqtt.retain==True and mqtt.qos == 0 and ip.dst==5.196.78.28")
 
     count = 0
-    ########################### Process requests
-    for pkt in packets:
-        if not hasattr(pkt, "mqtt") or not hasattr(pkt, "ip"):
-            continue  # Skip non-mqtt packets
-
-        mqtt_layer = pkt.mqtt
-
-        if (
-            # publish message
-            int(mqtt_layer.msgtype) == 3
-            # retain option set
-            and bool(mqtt_layer.retain)
-            # QoS "at most once"
-            and int(mqtt_layer.qos) == 0
-            # public mosquitto broker test.mosquitto.org
-            and pkt.ip.dst == "5.196.78.28"
-        ):
-            count += 1
-
+    for _ in packets:
+        count += 1
     print(count)
